fb653104df70d529e90a19c150307aa57987772a
Yoni Rapoport <yoni@fireflymigration.com>
2022-07-20 10:08:03 +0300

    added potgres

--- routes/api/_middleware.ts
+++ routes/api/_middleware.ts
@@ -2,62 +2,65 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
 import { Remult } from "remult";
 import { remultMiddleware } from "remult/remult-middleware";
+import { createPostgresConnection } from "../../postgres.ts";
 import { Task } from "../../shared/task.ts";
 
 const api = remultMiddleware({
-    entities: [Task],
-    initApi: async (remult: Remult) => {
-        const taskRepo = remult.repo(Task);
-        if (await taskRepo.count() === 0) {
-            await taskRepo.insert([
-                { title: "Task a" },
-                { title: "Task b", completed: true },
-                { title: "Task c" },
-                { title: "Task d" },
-                { title: "Task e", completed: true }
-            ]);
-        }
+  entities: [Task],
+  initApi: async (remult: Remult) => {
+    const taskRepo = remult.repo(Task);
+    if (await taskRepo.count() === 0) {
+      await taskRepo.insert([
+        { title: "Task a" },
+        { title: "Task b", completed: true },
+        { title: "Task c" },
+        { title: "Task d" },
+        { title: "Task e", completed: true },
+      ]);
     }
+  },
+  dataProvider: async () => {
+    var dbUrl = Deno.env.get("DATABASE_URL");
+    if (dbUrl) {
+      return createPostgresConnection({ connectionString: dbUrl });
+    }
+    return undefined;
+  },
 });
 
-
 export async function handler(
-    req: Request
+  req: Request,
 ) {
+  const url = new URL(req.url);
 
+  let query: any = {};
+  url.searchParams.forEach((val, key) => {
+    let current = query[key];
+    if (!current) {
+      query[key] = val;
+      return;
+    }
+    if (Array.isArray(current)) {
+      current.push(val);
+      return;
+    }
+    query[key] = [current, val];
+  });
 
-    const url = new URL(req.url);
+  const theReq = {
+    method: req.method,
+    originalUrl: req.url,
+    path: url.pathname,
+    body: req.bodyUsed ? await req.json() : undefined,
+    params: {},
+    query,
+  };
 
-    let query: any = {};
-    url.searchParams.forEach((val, key) => {
-
-        let current = query[key];
-        if (!current) {
-            query[key] = val;
-            return;
-        }
-        if (Array.isArray(current)) {
-            current.push(val);
-            return;
-        }
-        query[key] = [current, val]
+  return new Promise<Response>((res) => {
+    api(theReq, {
+      json: (data: any) => {
+        res(new Response(JSON.stringify(data)));
+      },
     });
-
-    const theReq = {
-        method: req.method,
-        originalUrl: req.url,
-        path: url.pathname,
-        body: req.bodyUsed ? await req.json() : undefined,
-        params: {},
-        query
-    };
-
-    return new Promise<Response>((res) => {
-        api(theReq, {
-            json: (data: any) => {
-                res(new Response(JSON.stringify(data)))
-            }
-        })
-    });
-
+  });
 }
