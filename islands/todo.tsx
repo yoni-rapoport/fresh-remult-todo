0976ba39fd86513c518ffdeb9ad62c838fac4a10
Yoni Rapoport <yoni@fireflymigration.com>
2022-07-20 11:03:54 +0300

    added crud

--- islands/todo.tsx
+++ islands/todo.tsx
@@ -6,26 +6,58 @@ import { Task } from "../shared/task.ts";
 import { Remult } from "remult";
 
 const remult = new Remult();
+
 const taskRepo = remult.repo(Task);
-function fetchTasks() {
-  return taskRepo.find();
+
+function fetchTasks(hideCompleted: boolean) {
+  return taskRepo.find({
+     limit: 20,
+     orderBy: { completed: "asc" },
+     where: { completed: hideCompleted ? false : undefined }
+  });
 }
 
 export default function Todo() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [hideCompleted, setHideCompleted] = useState(false);
 
   useEffect(() => {
-    fetchTasks().then(setTasks);
-  }, []);
+     fetchTasks(hideCompleted).then(setTasks);
+  }, [hideCompleted]);
+
+  const addTask = () => {
+     setTasks([...tasks, new Task()])
+  };
 
   return (
-    <div>
-      {tasks.map(task => (
-        <div key={task.id}>
-          <input type="checkbox" checked={task.completed} />
-          {task.title}
-        </div>
-      ))}
-    </div>
+     <div>
+        <input
+           type="checkbox"
+           checked={hideCompleted}
+           onClick={e => setHideCompleted(!hideCompleted)} /> Hide Completed
+        <hr />
+        {tasks.map(task => {
+           const handleChange = (values: Partial<Task>) => {
+              setTasks(tasks.map(t => t === task ? { ...task, ...values } : t));
+           }
+
+           const saveTask = () => {
+              taskRepo.save(task);
+           }
+
+           return (
+              <div key={task.id}>
+                 <input type="checkbox"
+                    checked={task.completed}
+                    onClick={e => handleChange({ completed: !task.completed })} />
+                 <input
+                    value={task.title}
+                    onInput={e => handleChange({ title: e.target.value })} />
+                 <button onClick={saveTask}>Save</button>
+              </div>
+           );
+        })}
+        <button onClick={addTask}>Add Task</button>
+     </div>
   );
 }
